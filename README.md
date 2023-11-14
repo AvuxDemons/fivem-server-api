80409526121a9189758605ab954095983c3a498e
avuxdemons <avuxdemons@gmail.com>
2023-11-14 09:26:32 +0700

    [+] 1.2.0 - CFX.re URL Support

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 Package for getting information about FiveM Server Using API
 
 ## Installation
+
 Add `fivem-server-api` to your existing project.
+
 ```
 npm i fivem-server-api
 ```
@@ -11,47 +13,82 @@ npm i fivem-server-api
 ## Documentation
 
 ```js
-const FiveM = require("fivem-server-api") // Import the Package.
-const server = new FiveM.Server('IP', 'PORT', 'ERROR', options)
-/*  The first argument is Server IP Address ( REQUIRED )
-    The second argument is Server Port      ( REQUIRED )
-    The third argument is Error Message     ( OPTIONAL )
-    The fourth argument is Timeout Handler  ( OPTIONAL )  */
+// Import the Package ( Required )
+const FiveM = require("fivem-server-api")
 
-// OPTIONAL , You can ignore this option
+// Options ( Optional )
 const options = {
-    timeout: 5000, // Default 5 seconds ( Convert To Milliseconds )
+    timeout: 5000, // Default 5000ms / 5 seconds ( Milliseconds )
+    errmsg: 'Error Occured', // Default 'Error Occured' ( String )
 }
 
-// How to use the function
-server.getServerStatus().then(data => console.log(data)) // Get Server Status [ Online / Offline ]
-server.getPlayers().then(data => console.log(data))      // Get Player Online Count
+// Create New Object ( Require )
+const server = new FiveM.Server('CFX.re URL / IP:PORT', options)
+/*  
+    The first argument is CFX.re Server URL / Server IP Address ( REQUIRED )
+    The second argument is Options                              ( Optional )
+    Example : 
+      const server = new FiveM.Server('205.178.183.132:50120', options)  ||  Using IP:PORT
+      const server = new FiveM.Server('cfx.re/join/my59jq', options)     ||  Using CFX.re Url
+      const server = new FiveM.Server('https://cfx.re/join/my59jq')      ||  Using CFX.re Url
+*/
 
-// Use More Than One Function
-Promise.all([server.getServerStatus(), server.getPlayers()])
-  .then(([serverStatus, players]) => {
+
+// How to use the function
+
+// Promise ( Single Func )
+server.getServerStatus().then(data => console.log(data))
+server.getPlayers().then(data => console.log(data))
+
+// PromiseAll ( Multiple Func )
+Promise.all([server.getServerStatus(), server.getPlayers()]).then(([serverStatus, players]) => {
     console.log(serverStatus);
     console.log(players);
-  })
-  .catch(error => {
+}).catch(error => {
     console.error(error);
-  });
+});
+
+// Async/Await ( Single Func )
+async function fetchServerStatus() {
+    const serverStatus = await server.getServerStatus();
+    console.log(serverStatus);
+}
+
+async function fetchPlayers() {
+    const players = await server.getPlayers();
+    console.log(players);
+}
+
+// Async/Await ( Multiple Func )
+async function fetchData() {
+    try {
+        const [serverStatus, players] = await Promise.all([server.getServerStatus(), server.getPlayers()]);
+        console.log(serverStatus);
+        console.log(players);
+    } catch (error) {
+        console.error(error);
+    }
+}
 ```
 
 ## **FUNCTION LIST**
 
 | FUNCTION                 | DETAIL                                                        | RESULT         |
 | :----------------------: | :-----------------------------------------------------------: | :------------: |
+| getServer()              | Get the whole server object                                   | (string)       |
 | getServerStatus()        | Server Status                                                 | (boolean)      |
+| getServerName()          | Get server name                                               | (string)       |
+| getServerDesc()          | Get server description                                        | (string)       |
 | getPlayers()             | Number of players online                                      | (number)       |
 | getPlayersAll()          | List all players                                              | (string/array) |
 | getMaxPlayers()          | Max players that are able to join the server                  | (number)       |
 | getResources()           | Get resource names of all server resources                    | (string/array) |
 | getTags()                | Get all server tags                                           | (string)       |
-| getServer()              | Get the whole server object                                   | (string)       |
 | getOnesync()             | See if the server has OneSync enabled                         | (boolean)      |
 | getLocale()              | The language of the server                                    | (string)       |
 | getGamename()            | Get the name of the server                                    | (string)       |
+| getSteamTicket()         | Is server require Steam ticket                                | (boolean)      |
+| getGameBuild()           | FiveM build version                                           | (boolean)      |
 | getEnhancedHostSupport() | ...                                                           | (boolean)      |
 | getlicenseKeyToken()     | The license key for the server                                | (string)       |
 | getScriptHookAllowed()   | See if the server supports external mod menus from the client | (boolean)      |
--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 const axios = require('axios');
 
 const DEFAULT_OPTIONS = {
-	timeout: 5000
+	timeout: 5000,
+	errmsg: 'Error Occured'
 };
 
 class Server {
-	constructor(ip, port, errmsg, options) {
-		if (!ip || !port) throw Error('Please provide an IP & Port.');
+	constructor(cfxre, options) {
+		if (!cfxre) throw Error('Please provide an CFX.re Url / IP:Port.');
 
-		this.ip = ip;
-		this.port = port;	
-		this.errmsg = errmsg || 'Error Occured';
+		if (cfxre.includes(":")) {
+			this.ip = cfxre;
+			console.log('Connected to server:', this.ip);
+		} else {
+			this.cfxre(cfxre);
+		}
+
 		this.options = Object.assign(DEFAULT_OPTIONS, options);
+	}
+
+	async cfxre(url) {
+		let request, modifiedUrl;
+		if (url.startsWith("cfx.re/join/")) {
+			request = "https://" + url;
+			modifiedUrl = url;
+		} else if (url.startsWith("https://cfx.re/join/")) {
+			request = url;
+			modifiedUrl = url.replace("https://", "");
+		} else {
+			request = "https://cfx.re/join/" + url;
+			modifiedUrl = "cfx.re/join/" + url;
+		}
+
+		try {
+			this.ip = (await axios.get(request)).headers["x-citizenfx-url"].replace("http://", "").replace("/", "");
+			console.log('Connected to server :', this.ip);
+		} catch (error) {
+			console.log(error);
+			return this.options.errmsg;
+		}
 	}
 
 	getPlayers() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/players.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/players.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let players = body.data;
 					send(players.length);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -31,13 +58,13 @@ class Server {
 	getPlayersAll() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/players.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/players.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let players = body.data;
 					send(players);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -45,7 +72,7 @@ class Server {
 	getServerStatus() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let server_status = {
 						online: true,
@@ -67,13 +94,13 @@ class Server {
 	getResources() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let resources = body.data.resources;
 					send(resources);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -81,13 +108,13 @@ class Server {
 	getOnesync() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let onesync = body.data.vars.onesync_enabled;
 					send(onesync);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -95,13 +122,13 @@ class Server {
 	getMaxPlayers() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let maxClients = body.data.vars.sv_maxClients;
 					send(maxClients);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -109,13 +136,13 @@ class Server {
 	getLocale() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let locale = body.data.vars.locale;
 					send(locale);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -123,13 +150,41 @@ class Server {
 	getGamename() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let gamename = body.data.vars.gamename;
 					send(gamename);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
+				});
+		});
+	}
+
+	getSteamTicket() {
+		return new Promise((send, err) => {
+			axios
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
+				.then(function (body) {
+					let steamTicket = body.data.requestSteamTicket;
+					send(steamTicket);
+				})
+				.catch((err) => {
+					send(this.options.errmsg);
+				});
+		});
+	}
+
+	getGameBuild() {
+		return new Promise((send, err) => {
+			axios
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
+				.then(function (body) {
+					let gameBuild = body.data.vars.sv_enforceGameBuild;
+					send(gameBuild);
+				})
+				.catch((err) => {
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -137,13 +192,13 @@ class Server {
 	getEnhancedHostSupport() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let enhancedHostSupport = body.data.vars.sv_enhancedHostSupport;
 					send(enhancedHostSupport);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -151,13 +206,13 @@ class Server {
 	getlicenseKeyToken() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let licenseKeyToken = body.data.vars.sv_licenseKeyToken;
 					send(licenseKeyToken);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -165,13 +220,13 @@ class Server {
 	getScriptHookAllowed() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let scriptHookAllowed = body.data.vars.sv_scriptHookAllowed;
 					send(scriptHookAllowed);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -179,13 +234,13 @@ class Server {
 	getTags() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let tags = body.data.vars.tags;
 					send(tags);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
 				});
 		});
 	}
@@ -193,13 +248,42 @@ class Server {
 	getServer() {
 		return new Promise((send, err) => {
 			axios
-				.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
 				.then(function (body) {
 					let server = body.data;
 					send(server);
 				})
 				.catch((err) => {
-					send(this.errmsg);
+					send(this.options.errmsg);
+				});
+		});
+	}
+
+	getServerName() {
+		return new Promise((send, err) => {
+			axios
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
+				.then(function (body) {
+					let serverName = body.data.vars.sv_projectName;
+					serverName = serverName.replace(/^\^\d+/, '');
+					send(serverName);
+				})
+				.catch((err) => {
+					send(this.options.errmsg);
+				});
+		});
+	}
+
+	getServerDesc() {
+		return new Promise((send, err) => {
+			axios
+				.get(`http://${this.ip}/info.json`, { timeout: this.options.timeout })
+				.then(function (body) {
+					let serverDesc = body.data.vars.sv_projectDesc;
+					send(serverDesc);
+				})
+				.catch((err) => {
+					send(this.options.errmsg);
 				});
 		});
 	}
